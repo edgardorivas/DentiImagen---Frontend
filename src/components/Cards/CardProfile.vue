661f7aac747cc431a8a01998545a9f2a063598bb
edgardo rivas <edgadorivas123@gmail.com>
2022-11-28 01:08:02 -0400

    cambios de apariencia

--- src/components/Cards/CardProfile.vue
+++ src/components/Cards/CardProfile.vue
@@ -21,7 +21,7 @@
               >
                 22
               </span>
-              <span class="text-sm text-blueGray-400">Friends</span>
+              <span class="text-sm text-blueGray-400">pacientes</span>
             </div>
             <div class="mr-4 p-3 text-center">
               <span
@@ -29,7 +29,7 @@
               >
                 10
               </span>
-              <span class="text-sm text-blueGray-400">Photos</span>
+              <span class="text-sm text-blueGray-400">odontodiagramas</span>
             </div>
             <div class="lg:mr-4 p-3 text-center">
               <span
@@ -37,43 +37,37 @@
               >
                 89
               </span>
-              <span class="text-sm text-blueGray-400">Comments</span>
+              <span class="text-sm text-blueGray-400">recivos</span>
             </div>
           </div>
         </div>
       </div>
       <div class="text-center mt-12">
         <h3
-          class="text-xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2"
+          class="text-xl font-semibold leading-normal  text-blueGray-700 mb-2"
         >
-          Jenna Stones
+          Ana Gonzzales
         </h3>
         <div
           class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase"
         >
           <i class="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
-          Los Angeles, California
+          Turmero, calle rivas
         </div>
         <div class="mb-2 text-blueGray-600 mt-10">
           <i class="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>
-          Solution Manager - Creative Tim Officer
+          Odontologo general
         </div>
-        <div class="mb-2 text-blueGray-600">
-          <i class="fas fa-university mr-2 text-lg text-blueGray-400"></i>
-          University of Computer Science
-        </div>
+        
       </div>
       <div class="mt-10 py-10 border-t border-blueGray-200 text-center">
         <div class="flex flex-wrap justify-center">
           <div class="w-full lg:w-9/12 px-4">
             <p class="mb-4 text-lg leading-relaxed text-blueGray-700">
-              An artist of considerable range, Jenna the name taken by
-              Melbourne-raised, Brooklyn-based Nick Murphy writes, performs and
-              records all of his own music, giving it a warm, intimate feel with
-              a solid groove structure. An artist of considerable range.
+             Especialista en dientes
             </p>
             <a href="javascript:void(0);" class="font-normal text-emerald-500">
-              Show more
+              Mostrar más
             </a>
           </div>
         </div>
--- src/components/Cards/CardSettings.vue
+++ src/components/Cards/CardSettings.vue
@@ -4,19 +4,19 @@
   >
     <div class="rounded-t bg-white mb-0 px-6 py-6">
       <div class="text-center flex justify-between">
-        <h6 class="text-blueGray-700 text-xl font-bold">My account</h6>
+        <h6 class="text-blueGray-700 text-xl font-bold">Mi cuenta</h6>
         <button
           class="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
           type="button"
         >
-          Settings
+          Configuraciones
         </button>
       </div>
     </div>
     <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
       <form>
         <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-          User Information
+          Informacion del usuario
         </h6>
         <div class="flex flex-wrap">
           <div class="w-full lg:w-6/12 px-4">
@@ -25,7 +25,7 @@
                 class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                 htmlFor="grid-password"
               >
-                Username
+                Nombre de usuario
               </label>
               <input
                 type="text"
@@ -40,7 +40,7 @@
                 class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                 htmlFor="grid-password"
               >
-                Email address
+                Correo electronico
               </label>
               <input
                 type="email"
@@ -55,7 +55,7 @@
                 class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                 htmlFor="grid-password"
               >
-                First Name
+              Nombre
               </label>
               <input
                 type="text"
@@ -70,7 +70,7 @@
                 class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                 htmlFor="grid-password"
               >
-                Last Name
+              Apellido
               </label>
               <input
                 type="text"
@@ -84,7 +84,7 @@
         <hr class="mt-6 border-b-1 border-blueGray-300" />
 
         <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-          Contact Information
+          Informacion de conctato
         </h6>
         <div class="flex flex-wrap">
           <div class="w-full lg:w-12/12 px-4">
@@ -93,7 +93,7 @@
                 class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                 htmlFor="grid-password"
               >
-                Address
+              Dirección
               </label>
               <input
                 type="text"
@@ -108,7 +108,7 @@
                 class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                 htmlFor="grid-password"
               >
-                City
+                Ciudad
               </label>
               <input
                 type="email"
@@ -123,7 +123,7 @@
                 class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                 htmlFor="grid-password"
               >
-                Country
+              País
               </label>
               <input
                 type="text"
@@ -138,7 +138,7 @@
                 class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                 htmlFor="grid-password"
               >
-                Postal Code
+                Codigo postal
               </label>
               <input
                 type="text"
@@ -152,7 +152,7 @@
         <hr class="mt-6 border-b-1 border-blueGray-300" />
 
         <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-          About Me
+          Sobre mí
         </h6>
         <div class="flex flex-wrap">
           <div class="w-full lg:w-12/12 px-4">
@@ -161,15 +161,14 @@
                 class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                 htmlFor="grid-password"
               >
-                About me
+              Sobre mí
               </label>
               <textarea
                 type="text"
                 class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                 rows="4"
               >
-                    A beautiful UI Kit and Admin for VueJS & Tailwind CSS. It is Free
-                    and Open Source.
+                    Odontologo de nacimiento
                   </textarea
               >
             </div>
