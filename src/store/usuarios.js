7c0e5606221c299d6a209fbc6c871f1fd92d34e2
Robert Gonzalez <robert.gonzalez@elitgon.com>
2023-01-01 17:46:25 -0400

    Agregando autenticaciones para los usuarios

--- src/store/usuarios.js
+++ src/store/usuarios.js
@@ -17,15 +17,18 @@ export default {
   },
   actions: {
     async obtenerListaDeUsuarios (context) {
+      const token = localStorage.getItem('token_acess')
       context.dispatch('getLoadingApp', true);
       try {
         const resultado = await axios({
           method: 'GET',
           baseURL: config.backend.baseURL,
           url: '/usuario',
+          headers: {
+            ['auth-token']: token,
+          }
         });
         context.commit('setusuarios', resultado.data)
-        
       } catch (error) {
         if (error.response) {
           alert(error);
