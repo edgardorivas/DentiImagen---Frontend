00061e65ea9b03a97aad5299a13d3eaf2e5de567
Robert Gonzalez <robert.gonzalez@elitgon.com>
2023-01-22 17:47:16 -0400

    Guardando cambios

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const configuracion = {
   backend: {
-    // baseURL: 'http://localhost:3000'
-    baseURL: 'http://192.168.10.101:3000'
+    baseURL: 'http://localhost:3000'
+    // baseURL: 'http://192.168.10.101:3000'
   },
   frontend: {
     title: 'Dentimagen',
