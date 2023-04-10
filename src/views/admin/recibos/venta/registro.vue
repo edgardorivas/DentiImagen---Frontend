6af3d685efa4f7ded664ce59ee14b4bf2c5e2231
edgardo rivas <edgadorivas123@gmail.com>
2023-04-10 03:45:07 -0400

    vista de registro de venta

--- src/views/admin/recibos/venta/registro.vue
+++ src/views/admin/recibos/venta/registro.vue
@@ -18,6 +18,9 @@
                         <el-step title="Servicios aplicados"></el-step>
                         
                         <el-step title="Materiales invertidos"></el-step>
+
+                        <el-step title="Vista previa"></el-step>
+
                         
                     </el-steps>
                 </div>
@@ -149,6 +152,7 @@
                                     </div>
                                 </div> -->
     
+                                <!-- tabla -->
                                 <el-table :data="datosVenta.servicios" class="w-full mt-10 ">
                                     <el-table-column prop="idServicio" label="Id"></el-table-column>
                                     <el-table-column v-for="dato in datosServicioTabla" :key="dato.id_servicio" class="text-center" label="Operaciones" >
@@ -191,23 +195,178 @@
                                 </div>
 
                                 <el-table v-if="datosMaterialesTabla.length" :data="datosMaterialesTabla" class="w-full mt-10 mb-10 ">
-                                     <el-table-column prop="id_recurso" label="Id"></el-table-column>
-                                     <el-table-column prop="nombre" label="Nombre "></el-table-column>
-                                     <el-table-column prop="descripcion" label="Descripcion "></el-table-column>
-                                     <el-table-column prop="disponible" label="Cantidad Disponible"></el-table-column>
-                                     <el-table-column fixed="right" label="Operaciones" width="170">
+                                    <el-table-column prop="id_recurso" label="Id"></el-table-column>
+                                    <el-table-column prop="nombre" label="Nombre "></el-table-column>
+                                    <el-table-column prop="descripcion" label="Descripcion "></el-table-column>
+                                    <el-table-column prop="disponible" label="Cantidad Disponible"></el-table-column>
 
-                                         <template>
+                                    <el-table-column  label="Cantidad Gastada" width="170">
+                                        <template slot-scope="scope">
+                                            <div v-for="datos in datosVenta.materiales" :key="datos.idMaterial">
+                                                <p v-if="datos.idMaterial == datosMaterialesTabla[scope.$index].id_recurso" >
+                                                    {{ datos.cantidadMaterial }}
+                                                </p>
+                                            </div>
+                                        </template>
+                                    </el-table-column>
 
+
+
+                                    <el-table-column fixed="right" label="Operaciones" width="170">
+                                        <template slot-scope="scope">
                                             <button type="button"
-                                                class="  md:w-1/2 py-2 text-red-600 bg-none transition duration-500 transform hover:-translate-y-1 hover:scale-100 uppercase rounded-md" @click="asociarServiciosIngresados(serviciosIngresados)">
+                                                class="  md:w-1/2 py-2 text-red-600 bg-none transition duration-500 transform hover:-translate-y-1 hover:scale-100 uppercase rounded-md"
+                                                    @click="eliminarAsociacionMaterial(scope.row.id_recurso)">
                                                     eliminar
                                             </button>
 
-                                         </template>
-
-                                     </el-table-column>
+                                        </template>
+                                    </el-table-column>
                                 </el-table> 
+                            </div>
+
+                            
+
+                        </div>
+
+                        <div v-if="active== 4" class="flex flex-wrap justify-around mt-5">
+                            <div class="w-11/12 mt-10 ml-48  m-0 p-0">
+                                
+                                <div class="block w-7/8 mb-10  p-6 bg-white border border-gray-200 rounded-lg shadow-md  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"> 
+                                    
+                                    <div class="flex justify-center">
+                                        <div class="rounded-t mb-0 px-4 py-3 border-0">
+                                            <p  class="font-semibold text-lg text-verdiAnderson uppercase">
+                                                Pago de Servicios
+                                            </p>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="flex mt-9 flex-wrap justify-around">
+                                        
+                                        <div class="w-1/3">
+                                            <p class=" mb-3 font-normal text-center text-gray-700 dark:text-gray-400">
+                                               <b>Datos de la clinica </b> 
+                                            </p>
+
+                                            <p class="font-normal text-gray-700 dark:text-gray-400">
+                                               <b>Nombre: </b> DENTIMAGEN C.A.
+                                            </p>
+                                            <p class="font-normal text-gray-700 dark:text-gray-400">
+                                               <b>Direccion: </b>Santiago mariño,turmero 
+                                            </p>
+                                            <p class="font-normal text-gray-700 dark:text-gray-400">
+                                               <b>Telefono: </b> 04126533015
+                                            </p>
+                                            
+                                        </div>
+
+
+                                        <div class="w-6/12 border-l-2 border-verdiAnderson pl-10">
+                                            <p class="font-normal mb-3 text-gray-700 dark:text-gray-400">
+                                                <b>Datos del Odontologo </b>
+                                            </p>
+                                            <div v-for="trabajador in usuarios.data" :key="trabajador.id_trabajador">
+                                                <div v-if="trabajador.id_trabajador == datosVenta.idTrabajador">
+
+                                                    <p class="font-normal text-gray-700 dark:text-gray-400">
+                                                        <b>Nombres: </b> {{ trabajador.nombre }} {{ trabajador.apellido }}
+                                                    </p>
+                                                    <p class="font-normal text-gray-700 dark:text-gray-400">
+                                                        <b>Telefono: </b> {{ trabajador.telefono }}
+                                                    </p>
+                                                    <p class="font-normal text-gray-700 dark:text-gray-400">
+                                                        <b>especializacion: </b> {{ trabajador.especializacion }}
+                                                    </p>
+                                                    <p class="font-normal text-gray-700 dark:text-gray-400">
+                                                        <b>Correo: </b> {{ trabajador.correo }}
+                                                    </p>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="w-11/12">
+                                        <el-divider>Datos del paciente</el-divider>
+                                    </div>
+
+                                    <div v-for="paciente in pacientes.data" :key="paciente.id_paciente" class="flex mt-9 pl-20 flex-wrap justify-start">
+                                        <div v-if="paciente.id_paciente == datosVenta.idPaciente" class="w-11/12">
+                                            
+                                            <p class="border  p-2 font-normal text-gray-700 dark:text-gray-400">
+                                                <b>Nombres: </b> {{ paciente.nombre_paciente }}
+                                            </p>
+                                            <p class=" border  p-2 font-normal text-gray-700 dark:text-gray-400">
+                                                <b>Apellido: </b> {{ paciente.apellido_paciente }}
+                                            </p>
+                                            <p class="border  p-2 font-normal text-gray-700 dark:text-gray-400">
+                                                <b>Edad: </b> {{ paciente.edad_paciente }} Años
+                                            </p>
+                                            <p class="border  p-2 font-normal text-gray-700 dark:text-gray-400">
+                                                <b>Cedula: </b> {{ paciente.cedula_paciente }}
+                                            </p>
+                                            <p class="border  p-2 font-normal text-gray-700 dark:text-gray-400">
+                                                <b>Telefono: </b> {{ paciente.telefono_paciente }}
+                                            </p>
+                                            <p class="border  p-2 font-normal text-gray-700 dark:text-gray-400">
+                                                <b>Correo: </b> {{ paciente.correo_paciente }}
+                                            </p>
+                                            <p class="border  p-2 font-normal text-gray-700 dark:text-gray-400">
+                                                <b>Motivo de la Consulta: </b> {{ paciente.motivo_consulta }}
+                                            </p>
+                                            <p class="border  p-2 font-normal text-gray-700 dark:text-gray-400">
+                                                <b>Genero: </b> {{ paciente.genero_paciente }}
+                                            </p>
+                                            <p class="border  p-2 font-normal text-gray-700 dark:text-gray-400">
+                                                <b>Dirreccion: </b> {{ paciente.direccion_paciente }}
+                                            </p>
+                                        </div>
+                                    </div>
+
+                                    <div class="w-11/12">
+                                        <el-divider>Servicio</el-divider>
+                                    </div>
+
+
+
+                                    <!-- tabla -->
+                                    <div class="relative overflow-x-auto mt-10">
+                                        <el-table :data="datosVenta.servicios" class="w-full mt-10 ">
+                                            <el-table-column prop="idServicio" label="Id"></el-table-column>
+                                            <el-table-column v-for="dato in datosServicioTabla" :key="dato.id_servicio" class="text-center" label="Operaciones" >
+                                                <template  >
+                                                    <p >
+                                                        {{ dato.nombre_servicio }}
+                                                    </p>
+                                                </template>
+                                            </el-table-column>
+                                            <el-table-column prop="costoServicio" label="Costo"></el-table-column>
+                                            <el-table-column prop="cantidadRealizadas" label="Cantidad"></el-table-column>
+                                        </el-table> 
+                                    </div>
+
+                                    <div class="  flex flex-row-reverse my-2 mt-2 mr-5">
+                                        <button @click="calculosRapidos()"
+                                            class="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                            type="button">
+                                            Calcular precio de pago
+                                        </button>
+                                    </div>
+            
+                                    <div v-if="totalSuma != 0" class="border mt-10 flex flex-wrap  border-solid border-gray-400 my-10 p-5">
+                                        <p class=" text-verdiAnderson w-1/2 pl-5 text-2xl">Total</p>
+                                        <p class="w-1/2 text-right text-2xl pl-5">{{ totalSuma }}</p>
+                                        
+                                    </div>
+                                    <br>
+                                    <div class="flex flex-wrap justify-around">
+                                        
+                                        <button :disabled="loading"
+                                            class="w-full md:w-1/3 bg-red-600 text-white transition duration-500 transform hover:-translate-y-1 hover:scale-100 uppercase py-2 rounded-md"
+                                            type="submit">Descargar PDF</button>
+                                    </div>
+                                </div>
                                 
     
                             </div>
@@ -219,14 +378,13 @@
 
                         </div>
 
+
                         <div class="flex flex-wrap  justify-end my-5">
                             <button type="button"
                                 class="w-full bg-none md:w-1/3  text-verdiAnderson transition duration-500 transform hover:-translate-y-1 hover:scale-100 uppercase py-2 rounded-md" @click="next">
                                 Siguiente ->
                             </button>
                         </div>
-
-{{ datosMaterialesTabla }}
 
                     </form>
                     <!-- Modal para agregar Servicio -->
@@ -389,6 +547,8 @@ export default {
                 idMaterial:null,
                 cantidadMaterial: null
             },
+            sumaTodoServicio : 0,
+            totalSuma: 0,
             datosServicioTabla:[],
             datosMaterialesTabla:[],
             centerDialogVisible: false,
@@ -397,7 +557,7 @@ export default {
     },
     methods: {
         next() {
-            if (this.active++ > 2) this.active = 1;
+            if (this.active++ > 3) this.active = 1;
         },
         asociarServiciosIngresados(datos){
             this.datosVenta.servicios.push(datos);
@@ -427,7 +587,22 @@ export default {
         },
         eliminarAsociacion(datos){
             console.log(datos)
+        },
+        eliminarAsociacionMaterial(datos){
+            this.datosVenta.materiales = this.datosVenta.materiales.filter(materiales => materiales.idServicio != datos)
+            this.datosMaterialesTabla = this.datosMaterialesTabla.filter(materiales => materiales.id_recurso != datos)
+
+            console.log(datos)
+        },
+        calculosRapidos(){
+            for (let index = 0; index < this.datosVenta.servicios.length; index++) {
+                this.sumaTodoServicio += (Number(this.datosVenta.servicios[index].costoServicio) * parseInt(this.datosVenta.servicios[index].cantidadRealizadas))
+            }
+            let iva = this.sumaTodoServicio * (parseInt(this.datosVenta.ivaVenta )/ 100);
+            this.totalSuma = this.sumaTodoServicio + iva;
+             
         }
+        
     },
     computed: {
         usuarios() {
@@ -443,6 +618,5 @@ export default {
             return this.$store.getters.getListaServicios;
         }
     },
-    
 };
 </script>
