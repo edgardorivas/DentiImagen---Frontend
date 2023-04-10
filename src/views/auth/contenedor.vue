0942ff9e9dd03c4787936d04faca53bc56313772
Robert Gonzalez <robert.gonzalez@elitgon.com>
2023-04-10 06:33:34 -0400

    Arreglando login

--- src/views/auth/contenedor.vue
+++ src/views/auth/contenedor.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="flex justify-center" id="container-auth">
-      <div class="flex flex-col h-screen">
+      <div class="flex flex-col">
         <div class="md:mt-40">
           <div class="justify-center w-1/3 m-auto">
             <img alt="Vue logo" width="300px" src="@/assets/logotipoEmpresa.png">
@@ -9,8 +9,10 @@
           <router-view />
         </div>
       </div>
-      <div class="hidden md:inline-block justify-center flex-1 bg-gradient-to-r from-verdiAnderson to-blue-500 h-screen">
-        <img src="../../assets/img/dentistas.svg" alt="dentistas">
+      <div class="hidden md:inline-block justify-center max-w-md bg-gradient-to-r from-verdiAnderson to-blue-500 h-auto">
+        <div class="flex h-screen justify-center items-center">
+          <img src="../../assets/img/dentistas.svg" alt="dentistas">
+        </div>
       </div>
     </div>
   </div>
