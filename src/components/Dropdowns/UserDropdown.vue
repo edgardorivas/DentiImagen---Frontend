fa638f5c05b8d040644fad51cf8614f9729d345e
edgardo rivas <edgadorivas123@gmail.com>
2023-04-20 02:06:04 -0400

    mi perfil

--- src/components/Dropdowns/UserDropdown.vue
+++ src/components/Dropdowns/UserDropdown.vue
@@ -1,40 +1,33 @@
 <template>
-  <div>
-    <a
-      class="text-blueGray-500 block"
-      href="#pablo"
-      ref="btnDropdownRef"
-      v-on:click="toggleDropdown($event)"
-    >
-      <div class="items-center flex">
-        <span
-          class="w-12 h-12 text-sm text-white bg-blueGray-200 inline-flex items-center justify-center rounded-full"
-        >
-          <img
-            alt="..."
-            class="w-full rounded-full align-middle border-none shadow-lg"
-            :src="'https://w7.pngwing.com/pngs/312/283/png-transparent-man-s-face-avatar-computer-icons-user-profile-business-user-avatar-blue-face-heroes-thumbnail.png'"
-          />
-        </span>
-      </div>
-    </a>
-    <div
-      ref="popoverDropdownRef"
-      class="bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
-      v-bind:class="{
-        hidden: !dropdownPopoverShow,
-        block: dropdownPopoverShow,
-      }"
-    >
-      <router-link to="/admin/miUsuario" class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">
-        Perfil
-      </router-link>
-      <div class="h-0 my-2 border border-solid border-blueGray-100" />
-      <router-link to="/logout" class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">
-        Cerrar Sesion
-      </router-link>
+    <div>
+        <a class="text-blueGray-500 block" href="#pablo" ref="btnDropdownRef" v-on:click="toggleDropdown($event)">
+            <div class="items-center flex">
+                <span
+                    class="w-12 h-12 text-sm text-white bg-blueGray-200 inline-flex items-center justify-center rounded-full">
+                    <img alt="..." class="w-full rounded-full align-middle border-none shadow-lg"
+                        :src="'https://w7.pngwing.com/pngs/312/283/png-transparent-man-s-face-avatar-computer-icons-user-profile-business-user-avatar-blue-face-heroes-thumbnail.png'" />
+                </span>
+            </div>
+        </a>
+        <div  ref="popoverDropdownRef"
+            class="bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48" v-bind:class="{
+                hidden: !dropdownPopoverShow,
+                block: dropdownPopoverShow,
+            }">
+            <div v-if="getmiUsuario && getmiUsuario.id_usuario" v-on:click="toggleDropdown">
+                <router-link :to="`/admin/usuarios/${getmiUsuario.id_usuario}`" 
+                    class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">
+                    Perfil
+                </router-link>
+            </div>
+            
+            <div class="h-0 my-2 border border-solid border-blueGray-100"> </div>
+            <router-link to="/logout"
+                class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">
+                Cerrar Sesion
+            </router-link>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -43,24 +36,29 @@ import { createPopper } from "@popperjs/core";
 import image from "@/assets/img/team-1-800x800.jpg";
 
 export default {
-  data() {
-    return {
-      dropdownPopoverShow: false,
-      image: image,
-    };
-  },
-  methods: {
-    toggleDropdown: function (event) {
-      event.preventDefault();
-      if (this.dropdownPopoverShow) {
-        this.dropdownPopoverShow = false;
-      } else {
-        this.dropdownPopoverShow = true;
-        createPopper(this.$refs.btnDropdownRef, this.$refs.popoverDropdownRef, {
-          placement: "bottom-start",
-        });
-      }
+    data() {
+        return {
+            dropdownPopoverShow: false,
+            image: image,
+        };
     },
-  },
+    methods: {
+        toggleDropdown: function (event) {
+            event.preventDefault();
+            if (this.dropdownPopoverShow) {
+                this.dropdownPopoverShow = false;
+            } else {
+                this.dropdownPopoverShow = true;
+                createPopper(this.$refs.btnDropdownRef, this.$refs.popoverDropdownRef, {
+                    placement: "bottom-start",
+                });
+            }
+        },
+    },
+    computed: {
+        getmiUsuario() {
+            return this.$store.getters.getmiUsuario;
+        }
+    }
 };
 </script>
