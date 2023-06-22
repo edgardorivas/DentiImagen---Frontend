a4870ed78475dfd40ac96f05bd63d3458dc071df
edgardo rivas <edgadorivas123@gmail.com>
2023-06-21 23:52:47 -0400

    cambios

--- src/views/admin/proveedores/editar.vue
+++ src/views/admin/proveedores/editar.vue
@@ -70,28 +70,7 @@
 
                                     </div>
                                     
-                                    <!--card con los items agregadas-->
-                                    <!-- <div class="flex flex-wrap py-2 mb-8 justify-around">
-                                        <div v-for="item in materialesProveedor.data" :key="item.id_recurso"   class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mx-1 mb-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                                            <div class="p-5">
-                                                <a href="#">
-                                                    <h5 class="  mb-5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ item.nombre_recurso }}</h5>
-                                                </a>
-                                                <p class="mb-3 text-left font-normal text-gray-700 dark:text-gray-400">{{ item.descripcion_recurso }}</p>
-                                                
-                                                <el-popconfirm confirm-button-text='Si, Eliminar' confirm-button-type="danger"
-                                                    cancel-button-text='No, Cancelar' icon="el-icon-info" icon-color="red"
-                                                    :title="`Estas seguro de eliminar el material ${item.nombre_recurso} asociado al proveedor ${proveedorDetalles.data[0].nombre_proveedor}?`"
-                                                    class="w-full md:w-1/3" @confirm="eliminarAsociacionItemProveedor(proveedorDetalles.data[0].id_provedor,item.id_recurso)">
-
-                                                    <button slot="reference" :disabled="loading"
-                                                        class="w-full bg-red-600 text-white transition duration-500 transform hover:-translate-y-1 hover:scale-100 uppercase py-2 rounded-md"
-                                                        type="button" >Borrar</button>
-                                                </el-popconfirm>
-                                            </div>
-                                        </div>
-                                    </div> -->
-
+                                    
                                     <!-- tabla para visualizar los items asociados al proveedor -->
                                     <div class="w-11/12 m-0 p-0">
                                         <el-table :data="materialesProveedor.data" class="w-full">
