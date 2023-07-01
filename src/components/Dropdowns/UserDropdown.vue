102d2efcd00422e85013722c999af115b27475d2
edgardo rivas <edgadorivas123@gmail.com>
2023-07-01 09:35:15 -0400

    edgardo

--- src/components/Dropdowns/UserDropdown.vue
+++ src/components/Dropdowns/UserDropdown.vue
@@ -5,22 +5,22 @@
                 <span
                     class="w-12 h-12 text-sm text-white bg-blueGray-200 inline-flex items-center justify-center rounded-full">
                     <img alt="..." class="w-full rounded-full align-middle border-none shadow-lg"
-                        :src="'https://w7.pngwing.com/pngs/312/283/png-transparent-man-s-face-avatar-computer-icons-user-profile-business-user-avatar-blue-face-heroes-thumbnail.png'" />
+                        src="../../assets/avatar_masculino.svg" />
                 </span>
             </div>
         </a>
-        <div  ref="popoverDropdownRef"
+        <div ref="popoverDropdownRef"
             class="bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48" v-bind:class="{
                 hidden: !dropdownPopoverShow,
                 block: dropdownPopoverShow,
             }">
             <div v-if="getmiUsuario && getmiUsuario.id_usuario" v-on:click="toggleDropdown">
-                <router-link :to="`/admin/usuarios/${getmiUsuario.id_usuario}`" 
+                <router-link :to="`/admin/usuarios/${getmiUsuario.id_usuario}`"
                     class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">
                     Perfil
                 </router-link>
             </div>
-            
+
             <div class="h-0 my-2 border border-solid border-blueGray-100"> </div>
             <router-link to="/logout"
                 class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">
--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -3,24 +3,34 @@
     <div class="w-full mb-12 xl:mb-0 px-4">
       <div class="relative flex flex-col min-w-0 w-full">
         <div class="flex flex-wrap justify-around px-3">
-          <div v-if="estadisticaPacientes && estadisticaPacientes.length" class="w-full sm:w-full md:w-2/5 mx-1 mb-3 shadow-lg rounded bg-white p-1">
+          <!-- {{ estadisticaPacientes }} -->
+
+          <div v-if="estadisticaPacientes && estadisticaPacientes.length"
+            class="w-full sm:w-full md:w-2/5 mx-1 mb-3 shadow-lg rounded bg-white p-1">
             <highcharts :options="returnEstadisticaPacientes(estadisticaPacientes)"></highcharts>
           </div>
-          <div v-if="estadisticaVentas && estadisticaVentas.length" class="w-full sm:w-full md:w-3/6 mx-1 mb-3 shadow-lg rounded bg-white p-1">
+          <div v-if="estadisticaVentas && estadisticaVentas.length"
+            class="w-full sm:w-full md:w-3/6 mx-1 mb-3 shadow-lg rounded bg-white p-1">
             <highcharts :options="returnEstadisticaVentas(estadisticaVentas)"></highcharts>
           </div>
-          <div v-if="estadisticaPresupuestos && estadisticaPresupuestos.length" class="w-full sm:w-full md:w-3/6 mx-1 mb-3 shadow-lg rounded bg-white p-1">
+          <div v-if="estadisticaPresupuestos && estadisticaPresupuestos.length"
+            class="w-full sm:w-full md:w-3/6 mx-1 mb-3 shadow-lg rounded bg-white p-1">
             <highcharts :options="returnEstadisticaPresupuestos(estadisticaPresupuestos)"></highcharts>
           </div>
-          <div v-if="estadisticaCompras && estadisticaCompras.length" class="w-full sm:w-full md:w-2/5 mx-1 mb-3 shadow-lg rounded bg-white p-1">
+          <div v-if="estadisticaCompras && estadisticaCompras.length"
+            class="w-full sm:w-full md:w-2/5 mx-1 mb-3 shadow-lg rounded bg-white p-1">
             <highcharts :options="returnEstadisticaCompras(estadisticaCompras)"></highcharts>
           </div>
+          <!-- <div class="w-full h-11/12">
+            <CardLineChart ></CardLineChart>
+          </div> -->
         </div>
       </div>
       <div class="flex flex-wrap mt-4 ">
         <div class="w-full mb-12 xl:mb-0 px-4">
           <div class="relative  flex  flex-col min-w-0 break-words w-50 mb-6">
-            <img src="../../assets/img/dashboard.svg" style="display: block;margin-left: auto;margin-right: auto; " alt="">
+            <img src="../../assets/img/dashboard.svg" style="display: block;margin-left: auto;margin-right: auto; "
+              alt="">
           </div>
         </div>
       </div>
@@ -28,174 +38,190 @@
   </div>
 </template>
 <script>
-  import config from './../../config';
-  export default {
-    name: "dashboard-page",
-    metaInfo: {
-      title: config.frontend.title,
-      titleTemplate: '%s | Inicio',
+
+import CardLineChart from '@/components/Cards/CardLineChart.vue';
+
+
+
+
+
+import config from './../../config';
+export default {
+  name: "dashboard-page",
+  metaInfo: {
+    title: config.frontend.title,
+    titleTemplate: '%s | Inicio',
+  },
+  components: {
+    // CardLineChart
+  },
+
+
+  created() {
+    this.obtenerEstadisticas()
+  },
+  data() {
+    return {
+      payloadSearch: {
+        fechaInicio: new Date(new Date().getFullYear(), new Date().getMonth() - 1, 1),
+        fechaFinal: new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0),
+      },
+
+
+
+
+    }
+  },
+  methods: {
+    obtenerEstadisticas() {
+      this.$store.dispatch('actionEstadisticas', this.payloadSearch);
     },
-    created() {
-      this.obtenerEstadisticas()
-    },
-    data() {
-      return {
-        payloadSearch: {
-          fechaInicio: new Date(new Date().getFullYear(), new Date().getMonth() - 1, 1),
-          fechaFinal: new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0),
-        }
+    returnEstadisticaPacientes(payload) {
+      const resultado = {
+        title: { text: `Registro de pacientes por fecha ${this.payloadSearch.fechaInicio} al ${this.payloadSearch.fechaFinal}` },
+        legend: {
+          enabled: true,
+        },
+        credits: false,
+        plotOptions: {
+          line: {
+            dataLabels: { enabled: true },
+            enableMouseTracking: true,
+          },
+        },
+        xAxis: {
+          categories: ['Pacientes por fecha'],
+        },
+        series: [
+          {
+            data: [0],
+            name: "Promedios de pacientes",
+            label: "Actual"
+          },
+        ],
+      };
+      for (const estadistica of payload) {
+        const objetoSeries = {
+          name: `${estadistica.fecha_creacion}`,
+          data: [0, Number(estadistica.count)]
+        };
+        resultado.series.push(objetoSeries);
       }
+      return resultado;
     },
-    methods: {
-      obtenerEstadisticas() {
-        this.$store.dispatch('actionEstadisticas', this.payloadSearch);
-      },
-      returnEstadisticaPacientes(payload) {
-        const resultado = {
-          title: { text: `Registro de pacientes por fecha ${this.payloadSearch.fechaInicio} al ${this.payloadSearch.fechaFinal}` },
-          legend: {
-            enabled: true,
+    returnEstadisticaVentas(payload) {
+      const resultado = {
+        title: { text: `Registro de ventas por fecha ${this.payloadSearch.fechaInicio} al ${this.payloadSearch.fechaFinal}` },
+        legend: {
+          enabled: true,
+        },
+        credits: { enabled: true },
+        plotOptions: {
+          line: {
+            dataLabels: { enabled: true },
+            enableMouseTracking: true,
           },
-          credits: { enabled: true },
-          plotOptions: {
-            line: {
-              dataLabels: { enabled: true },
-              enableMouseTracking: true,
-            },
+        },
+        xAxis: {
+          categories: ['Ventas por fecha'],
+        },
+        series: [
+          {
+            data: [0],
+            name: "Promedios de ventas",
+            label: "Actual"
           },
-          xAxis: {
-            categories: ['Pacientes por fecha'],
-          },
-          series: [
-            {
-              data: [0],
-              name: "Promedios de pacientes",
-              label: "Actual"
-            },
-          ],
+        ],
+      };
+      for (const estadistica of payload) {
+        const objetoSeries = {
+          name: `${estadistica.nombre_servicio}`,
+          data: [0, Number(estadistica.cantidad_vendida)]
         };
-        for (const estadistica of payload) {
-          const objetoSeries = {
-            name: `${estadistica.fecha_creacion.slice(0, 10)}`,
-            data: [0, Number(estadistica.count)]
-          };
-          resultado.series.push(objetoSeries);
-        }
-        return resultado;
-      },
-      returnEstadisticaVentas(payload) {
-        const resultado = {
-          title: { text: `Registro de ventas por fecha ${this.payloadSearch.fechaInicio} al ${this.payloadSearch.fechaFinal}` },
-          legend: {
-            enabled: true,
-          },
-          credits: { enabled: true },
-          plotOptions: {
-            line: {
-              dataLabels: { enabled: true },
-              enableMouseTracking: true,
-            },
-          },
-          xAxis: {
-            categories: ['Ventas por fecha'],
-          },
-          series: [
-            {
-              data: [0],
-              name: "Promedios de ventas",
-              label: "Actual"
-            },
-          ],
-        };
-        for (const estadistica of payload) {
-          const objetoSeries = {
-            name: `${estadistica.nombre_servicio}`,
-            data: [0, Number(estadistica.cantidad_vendida)]
-          };
-          resultado.series.push(objetoSeries);
-        }
-        return resultado;
-      },
-      returnEstadisticaPresupuestos(payload) {
-        const resultado = {
-          title: { text: `Registro de presupuestos por fecha ${this.payloadSearch.fechaInicio} al ${this.payloadSearch.fechaFinal}` },
-          legend: {
-            enabled: true,
-          },
-          credits: { enabled: true },
-          plotOptions: {
-            line: {
-              dataLabels: { enabled: true },
-              enableMouseTracking: true,
-            },
-          },
-          xAxis: {
-            categories: ['Presupuestos por fecha'],
-          },
-          series: [
-            {
-              data: [0],
-              name: "Promedios de presupuestos",
-              label: "Actual"
-            },
-          ],
-        };
-        for (const estadistica of payload) {
-          const objetoSeries = {
-            name: `${estadistica.estado_compra}`,
-            data: [0, Number(estadistica.count)]
-          };
-          resultado.series.push(objetoSeries);
-        }
-        return resultado;
-      },
-      returnEstadisticaCompras(payload) {
-        const resultado = {
-          title: { text: `Registro de compras por fecha ${this.payloadSearch.fechaInicio} al ${this.payloadSearch.fechaFinal}` },
-          legend: {
-            enabled: true,
-          },
-          credits: { enabled: true },
-          plotOptions: {
-            line: {
-              dataLabels: { enabled: true },
-              enableMouseTracking: true,
-            },
-          },
-          xAxis: {
-            categories: ['Compras por fecha'],
-          },
-          series: [
-            {
-              data: [0],
-              name: "Promedios de compras",
-              label: "Actual"
-            },
-          ],
-        };
-        for (const estadistica of payload) {
-          const objetoSeries = {
-            name: `${estadistica.fecha_compra.slice(0, 10)}`,
-            data: [0, Number(estadistica.count)]
-          };
-          resultado.series.push(objetoSeries);
-        }
-        return resultado;
-      },
+        resultado.series.push(objetoSeries);
+      }
+      return resultado;
     },
-    computed: {
-      estadisticaPacientes() {
-        return this.$store.getters.getEstadisticaPaciente;
-      },
-      estadisticaVentas() {
-        return this.$store.getters.getEstadisticaVenta;
-      },
-      estadisticaPresupuestos() {
-        return this.$store.getters.getEstadisticaPresupuesto;
-      },
-      estadisticaCompras() {
-        return this.$store.getters.getEstadisticaCompra;
-      },
+    returnEstadisticaPresupuestos(payload) {
+      const resultado = {
+        title: { text: `Registro de presupuestos por fecha ${this.payloadSearch.fechaInicio} al ${this.payloadSearch.fechaFinal}` },
+        legend: {
+          enabled: true,
+        },
+        credits: { enabled: true },
+        plotOptions: {
+          line: {
+            dataLabels: { enabled: true },
+            enableMouseTracking: true,
+          },
+        },
+        xAxis: {
+          categories: ['Presupuestos por fecha'],
+        },
+        series: [
+          {
+            data: [0],
+            name: "Promedios de presupuestos",
+            label: "Actual"
+          },
+        ],
+      };
+      for (const estadistica of payload) {
+        const objetoSeries = {
+          name: `${estadistica.estado_compra}`,
+          data: [0, Number(estadistica.count)]
+        };
+        resultado.series.push(objetoSeries);
+      }
+      return resultado;
     },
-  };
+    returnEstadisticaCompras(payload) {
+      const resultado = {
+        title: { text: `Registro de compras por fecha ${this.payloadSearch.fechaInicio} al ${this.payloadSearch.fechaFinal}` },
+        legend: {
+          enabled: true,
+        },
+        credits: { enabled: true },
+        plotOptions: {
+          line: {
+            dataLabels: { enabled: true },
+            enableMouseTracking: true,
+          },
+        },
+        xAxis: {
+          categories: ['Compras por fecha'],
+        },
+        series: [
+          {
+            data: [0],
+            name: "Promedios de compras",
+            label: "Actual"
+          },
+        ],
+      };
+      for (const estadistica of payload) {
+        const objetoSeries = {
+          name: `${estadistica.fecha_compra.slice(0, 10)}`,
+          data: [0, Number(estadistica.count)]
+        };
+        resultado.series.push(objetoSeries);
+      }
+      return resultado;
+    },
+  },
+  computed: {
+    estadisticaPacientes() {
+      return this.$store.getters.getEstadisticaPaciente;
+    },
+    estadisticaVentas() {
+      return this.$store.getters.getEstadisticaVenta;
+    },
+    estadisticaPresupuestos() {
+      return this.$store.getters.getEstadisticaPresupuesto;
+    },
+    estadisticaCompras() {
+      return this.$store.getters.getEstadisticaCompra;
+    },
+  },
+};
 </script>
